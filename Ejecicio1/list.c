04230c693e4457170f741701072ffb2082cf6489
DanParSeg <daniel.parra.segovia@gmail.com>
2020-01-31 19:38:49 +0100

    newList debería funcionar

--- Ejecicio1/list.c
+++ Ejecicio1/list.c
@@ -1,13 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "list.h"
-/*
-List
+
+List *
 newList(){
-
-
+  static List * l;
+  l = malloc(sizeof(List));
+  //TODO: if(l==NULL){} //por si no tiene memoria
+  l->size=0;
+  return l;
 }
-
+/*
 void
 delete(List l){
 
--- Ejecicio1/list.h
+++ Ejecicio1/list.h
@@ -12,18 +12,18 @@ struct Coor{
 typedef struct Node Node;
 struct Node{
   Coor data;
-  Node* next;
-  Node* prev;
+  Node * next;
+  Node * prev;
 };
 
 typedef struct List List;
 struct List{
   int size;
-  Node* head;
-  Node* tail;
+  Node * head;
+  Node * tail;
 };
 
-List newList(void);//crear una lista vacía
+List * newList(void);//crear una lista vacía
 void delete(List l);//destruir la lista
 
 char isEmpty(List l);//preguntar si una lista está vacía
@@ -36,5 +36,5 @@ void first(List l);//determinar el primer elemento (o la "cabeza") de una lista
 void last(List l);//determinar el último elemento (o la "cola") de una lista
 
 Coor searchXY(List l, int x, int y);//preguntar si una coordenada está en la lista pasando sus dos componentes (x,y),  y si es así, conseguir el puntero a la misma (si hay varias, la primera de la lista)
-Coor searchTag(List l, char *tag[]);//preguntar si una coordenada está en la lista pasando su etiqueta, y si es así, conseguir el puntero a la misma (si hay varias, la primera de la lista)
-void erase(List l, Coor* c);//eliminar una coordenada de la lista pasando su puntero
+Coor searchTag(List l, char * tag[]);//preguntar si una coordenada está en la lista pasando su etiqueta, y si es así, conseguir el puntero a la misma (si hay varias, la primera de la lista)
+void erase(List l, Coor * c);//eliminar una coordenada de la lista pasando su puntero
