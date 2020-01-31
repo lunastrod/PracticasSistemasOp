26065f4bc4bc930d5ed48f9c036a52272d4ef443
DanParSeg <daniel.parra.segovia@gmail.com>
2020-01-31 20:31:53 +0100

    creo que addleft funciona

--- Ejecicio1/list.c
+++ Ejecicio1/list.c
@@ -35,3 +35,43 @@ char isEmpty(List * l){return l->size==0;}
 int size(List * l){return l->size;}
 Coor first(List * l){return l->head->data;}
 Coor last(List * l){return l->tail->data;}
+
+Node * newNode(){
+  static Node * n;
+  n = malloc(sizeof(Node));
+  n->next=NULL;
+  n->prev=NULL;
+  return n;
+}
+
+void addLeft(List * l){
+  Node * n = newNode();
+  //TODO: meter datos en el nodo
+  switch (l->size) {
+    case 0:
+      l->head=n;
+      l->tail=n;
+      break;
+    default:
+      n->prev=l->tail;
+      l->tail->next=n;
+      l->tail=n;
+  }
+  l->size++;
+}
+void addRight(List * l){
+
+}
+
+Coor searchXY(List * l, int x, int y){
+
+
+}
+Coor searchTag(List * l, char * tag[]){
+
+
+}
+void erase(List * l, Coor * c){
+
+
+}
