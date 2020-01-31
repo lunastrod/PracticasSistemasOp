0d6f3d66351004fcf4459089670be61e8a3b4965
DanParSeg <daniel.parra.segovia@gmail.com>
2020-01-31 18:45:58 +0100

    tagSize, List ESTABLE

--- Ejecicio1/list.h
+++ Ejecicio1/list.h
@@ -1,16 +1,27 @@
+enum{
+  tagSize=255
+};
+
 
 typedef struct Coor Coor;
 struct Coor{
   int x;
   int y;
-  char tag[];
+  char tag[tagSize];
 };
 
 typedef struct Node Node;
 struct Node{
   Coor data;
-  Node next;
-  Node prev;
+  Node* next;
+  Node* prev;
 };
+
+typedef struct List List;
+struct List{
+  Node* first;
+  Node* last;
+};
+
 
 void holaMundo();
--- Ejecicio1/main.c
+++ Ejecicio1/main.c
@@ -6,7 +6,10 @@ int
 main(int argc, char *argv[])
 {
   Coor c = {10,11};
-  c.tag="hola";
-  printf("%s :(%d, %d)\n",c.tag,c.x,c.y);
+  c.tag[0]='h';
+  c.tag[1]='o';
+  c.tag[2]='l';
+  c.tag[3]='a';
+  printf("%s:(%d, %d)\n",c.tag,c.x,c.y);
   exit(EXIT_SUCCESS);
 }
