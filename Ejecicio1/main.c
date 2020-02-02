d118d00f9068ce02728d1d53cf1422dd720c4c65
DanParSeg <daniel.parra.segovia@gmail.com>
2020-02-02 18:26:48 +0100

    comentarios en el main

--- Ejecicio1/main.c
+++ Ejecicio1/main.c
@@ -3,17 +3,17 @@
 #include "list.h"
 
 enum{
-  listSize=6
+  listSize=10
 };
 
 int main(int argc, char *argv[]){
   Coor c={0,0,""};
   int a;
-  List * l = newList();
+  List * l = newList();//creo lista, pila y cola
   Stack * s= newStack();
   Queue * q= newQueue();
 
-  for(a=0; a<=listSize; a++){
+  for(a=0; a<=listSize; a++){//las lleno de coordenadas ascendentes
     c.x=a;
     c.y=a;
     addRight(l,c);
@@ -27,18 +27,18 @@ int main(int argc, char *argv[]){
   printf("queue: ");
   printQueue(q);
 
-  for(a=0; a<=listSize; a+=2){
+  for(a=0; a<=listSize; a+=2){//quito los impares de la lista
     erase(l,searchXY(l,a,a));
   }
   printf("\nodds:      ");
   printList(l);
   printf("reverse:   ");
-  for(a=0; a<=listSize; a++){
+  for(a=0; a<=listSize; a++){//imprimo en sentido inverso la pila
     c=pop(s);
     printf("%s(%d,%d) ",c.tag,c.x,c.y);
   }
   printf("\nascending: ");
-  for(a=0; a<=listSize; a++){
+  for(a=0; a<=listSize; a++){//imprimo en orden ascendente la cola
     c=dequeue(q);
     printf("%s(%d,%d) ",c.tag,c.x,c.y);
   }
@@ -51,7 +51,7 @@ int main(int argc, char *argv[]){
   printf("queue: ");
   printQueue(q);
 
-  deleteList(l);
+  deleteList(l);//destruyo la lista, pila y cola
   deleteStack(s);
   deleteQueue(q);
 }
