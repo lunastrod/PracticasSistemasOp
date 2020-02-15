122bd41edac90b439d4136c9bb7d91fa08f5bb0e
astrod <daniel.parra.segovia@gmail.com>
2020-02-15 15:19:31 +0100

    preguntar dudas, pero funciona

--- Ejercicio3/main.c
+++ Ejercicio3/main.c
@@ -4,41 +4,18 @@
 #include <string.h>
 
 /*
-Escriba un programa en C llamado execargs que ejecute todos los comandos que se le pasen como argumentos.
-El primer argumento debe ser el número de segundos que hay que esperar entre la ejecución de cada uno.
-El resto de argumentos que se le pasan al programa serán rutas de ejecutables,
-opcionalmente con argumentos para ese comando si la string contiene espacios.
-Se considerará error si el programa no recibe al menos dos argumentos o el primero no es un entero.
-En caso de error, se debe avisar como corresponda y acabar la ejecución (no se deben ejecutar los programas restantes).
-Para esperar un número de segundos, convertir de string a entero y partir una string en trozos usando un separador, se recomienda la lectura de:
+TODO: rutas??? El resto de argumentos que se le pasan al programa serán rutas de ejecutables,
+TODO: cómo??? En caso de error, se debe avisar como corresponda y acabar la ejecución (no se deben ejecutar los programas restantes).
 
-man 3 sleep
-man 3 atoi
-man 3 strtol
-man 3 strtok_r
-
-Un ejemplo de ejecución es el siguiente:
-
-$> ./execargs 2 '/bin/echo hola' '/bin/echo adios'
-hola
-adios
-$> ./execargs
-usage: execargs secs command [command ...]
-$> ls
-execargs
-execargs.c
-execargs.o
+TODO: no sería /bin/echo???
 $> ./execargs 1 /bin/ls 'echo ya esta'
 execargs
 execargs.c
 execargs.o
 ya esta
-$>
 */
 
 enum{
-  FALSE,
-  TRUE,
   ARG_MAX=255//max number of arguments
 };
 
@@ -50,15 +27,14 @@ void argverror() {
 int exec_command(char * input){
   //example: exec_command("/bin/echo a a a");
   //will exec "/bin/echo" with 3 args: "a" "a" "a"
+  char * rest = input;
   char * command;
   char * args[ARG_MAX];
-  char * rest = input;
   int i=1;
 
   command = strtok_r(rest, " ", &rest);
   args[0]=command;
-
-  //tokenizo input en args y añado un NULL al final de args
+  //tokens input in args and adds a NULL at the end
   while ((args[i] = strtok_r(rest, " ", &rest))){
     //printf("%s, i=%d\n", args[i],i);
     i++;
@@ -67,17 +43,14 @@ int exec_command(char * input){
   return execv(command,args);
 }
 
-int parseint(char * str){
-  char ** not_parsable_substring=malloc((strlen(str)+1)*sizeof(char));
-  int n=strtol(str,not_parsable_substring,10);
-  if(strlen(*not_parsable_substring)>0){//strtol hasnt finished parsing
-    //str is not parsable to int
-    fprintf(stderr, "not_parsable_substring=%s\n", *not_parsable_substring);
-    free(not_parsable_substring);
+int parseint(char * input){
+  char * rest = input;
+  int n =strtol(rest,&rest,10);
+
+  if(strlen(rest)>0){//strtol hasnt finished parsing
+    //fprintf(stderr, "not_parsable_substring=%s\n", rest);
     argverror();
   }
-  free(not_parsable_substring);
-  //printf("n=%d\n",n);
   return n;
 }
 
