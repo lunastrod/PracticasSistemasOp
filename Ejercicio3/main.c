6c140de6bf8781aa33170e80dd7fab99f8b9c553
astrod <daniel.parra.segovia@gmail.com>
2020-02-19 13:36:54 +0100

    no se puede usar wait

--- Ejercicio3/main.c
+++ Ejercicio3/main.c
@@ -7,27 +7,21 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
-TODO: rutas??? El resto de argumentos que se le pasan al programa serán rutas de ejecutables,
-
-TODO: no sería /bin/echo???
-$> ./execargs 1 /bin/ls 'echo ya esta'
-execargs
-execargs.c
-execargs.o
-ya esta
-*/
+const char USAGE_ERROR[] =    "execargs secs command [command ...]";
+const char PARSEINT_ERROR[] = "is not an integer value";
+const char EXEC_ERROR[] =     "command not found";
+const char FORK_ERROR[] =     "can't fork";
 
 enum{
-  ARG_MAX=255//max number of arguments
+  ARG_MAX=256//max number of arguments
 };
 
 void argverror() {
-  fprintf(stderr, "execargs secs command [command ...]\n");
+  fprintf(stderr, "%s\n", USAGE_ERROR);
   exit(EXIT_FAILURE);
 }
 
-int exec_command(char * input){
+void exec_command(char * input){
   //example: exec_command("/bin/echo a a a");
   //will exec "/bin/echo" with 3 args: "a" "a" "a"
   char * rest = input;
@@ -39,11 +33,12 @@ int exec_command(char * input){
   args[0]=command;
   //tokens input in args and adds a NULL at the end
   while ((args[i] = strtok_r(rest, " ", &rest))){
-    //printf("%s, i=%d\n", args[i],i);
     i++;
   }
   args[i]=NULL;
-  return execv(command,args);
+  execv(command,args);
+  fprintf(stderr, "'%s' %s\n", command, EXEC_ERROR);
+  argverror();
 }
 
 int parseint(char * input){
@@ -51,7 +46,7 @@ int parseint(char * input){
   int n =strtol(rest,&rest,10);
 
   if(strlen(rest)>0){//strtol hasnt finished parsing
-    //fprintf(stderr, "not_parsable_substring=%s\n", rest);
+    fprintf(stderr, "'%s' %s\n", input, PARSEINT_ERROR);
     argverror();
   }
   return n;
@@ -60,19 +55,18 @@ int parseint(char * input){
 int main(int argc, char * argv[]){
   int i;
   int secs;
-  int exit_code;
   if(argc<=2){
     argverror();
   }
-  secs = parseint(argv[1]);//might call argverror() if not parsable
+  secs = parseint(argv[1]);//calls argverror() if not parsable
 
   for(i=2; i<argc; i++){
-    if(fork() == 0){
-      exec_command(argv[i]);
-      argverror();//TODO: si el hijo llega aquí, matar al padre también
-    }
-    if(wait(&exit_code)!=-1 && WEXITSTATUS(exit_code)==EXIT_FAILURE){
-      exit(EXIT_FAILURE);
+    switch(fork()){
+      case 0:
+        exec_command(argv[i]);//calls argverror() if error
+      case -1:
+        fprintf(stderr, "%s\n", FORK_ERROR);
+        argverror();
     }
     sleep(secs);
   }
